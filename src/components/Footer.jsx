258cf237b3c477f513741afba9fe32ba33118571
pedrol-9 <psanabria999@gmail.com>
2024-07-29 09:07:24 -0500

    fixing the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,18 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <footer className="flex flex-wrap bg-gray-800 p-2 text-white">
+    <footer className="flex flex-wrap bg-gray-800 text-white py-4 mt-10">
       <div className="w-full">
-        <picture className="w-full flex justify-center space-x-4">
+        <picture className="w-full flex justify-center items-center space-x-4">
           <a href="https://www.linkedin.com/in/pedrosanabria9" target="_blank">
             <img src="/linkedin.png" alt="linkedIn logotype" className="h-10" />
           </a>
           <a href="https://discord.com/users/pedrosanabria9" target="_blank">
             <img src="/discord.png" alt="discord logotype" className="h-10" />
           </a>
-
+          <a href="mailto:psanabria999@gmail.com" target="_blank">
+            <img src="/gmail.png" alt="discord logotype" className="h-10" />
+          </a>
         </picture>
       </div>
     </footer>
